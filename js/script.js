b578c14159b08f4e6af50071d3a2afe3daed6db0
Yaroslava Terletska <yaroslava1408@ukr.net>
2023-03-19 17:54:42 +0100

    get 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function titleClickHandler(event) {
+  event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
+  console.log('Link was clicked!'); 
    
   
   
@@ -11,18 +12,22 @@ const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add ('active');
-  console.log('clickedElement:', clickedElement);
+  /*console.log('clickedElement:', clickedElement);*/
+  
 
   /*  [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
   for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+const articleSelector = clickedElement.getAttribute('href'); 
+console.log (articleSelector);
+
+  /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
 }

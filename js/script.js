409e07619f95a5b2b6964c59b1dfc62fdd43c0dd
Yaroslava Terletska <yaroslava1408@ukr.net>
2023-03-21 23:36:35 +0100

    Add Eslint

--- js/script.js
+++ js/script.js
@@ -3,15 +3,13 @@
 function titleClickHandler(event) {
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!'); 
-  
-    
-      
+  console.log('Link was clicked!');
+
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
-    for(let activeLink of activeLinks){
+  for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
-    }
+  }
 
   /* [DONE] add class 'active' to the clicked link */
     clickedElement.classList.add ('active');
